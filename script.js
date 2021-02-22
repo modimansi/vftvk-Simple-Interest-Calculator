ab780efa4c201ac32d49f581fdfba1d25c5efa3a
Mansi <mansi.modi@streebo.com>
2021-02-22 20:09:02 +0530

    fix: fixed according to requirements

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function compute() {
     var rate = parseFloat(document.getElementById("rate").value); 
     var years = parseInt(document.getElementById("years").value); 
 
-    if(principal <= 0) {
+    if(principal <= 0 || isNaN(principal)) {
         alert("Please enter a positive number");
+        document.getElementById("principal").focus();
     } else {
         var interest = principal * years * rate / 100;
         var amount = principal + interest;
@@ -12,8 +13,8 @@ function compute() {
         yearInFuture = parseInt(2021 + years);
 
         result = document.getElementById("result");
-        result.innerHTML = "If you deposit " + principal + ",<br/>at an interest rate of " + rate + "%.<br/>You will receive an amount of " + interest + ",<br/>in the year " + yearInFuture;
-
+        result.innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>at an interest rate of <mark>" + rate + "%</mark>.<br/>You will receive an amount of <mark>" + interest + "</mark>,<br/>in the year <mark>" + yearInFuture + "</mark>";
+        
     }
     
 }
