3cd29960a7bbf5718ae1030fe6140e58b9879532
Mansi <mansi.modi@streebo.com>
2021-02-22 19:01:38 +0530

    DEV: Modified HTML, CSS, Added JS

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value); 
+    var years = parseInt(document.getElementById("years").value); 
+
+    if(principal <= 0) {
+        alert("Please enter a positive number");
+    } else {
+        var interest = principal * years * rate / 100;
+        var amount = principal + interest;
+
+        yearInFuture = parseInt(2021 + years);
+
+        result = document.getElementById("result");
+        result.innerHTML = "If you deposit " + principal + ",<br/>at an interest rate of " + rate + "%.<br/>You will receive an amount of " + interest + ",<br/>in the year " + yearInFuture;
+
+    }
     
 }
-        
+
+function displayRateValue() {
+    var rate = document.getElementById("rate").value;
+    document.getElementById("rateVal").innerHTML = rate + "%";
+
+}
